0f210d62a8ab2362f30f11631e7100d1f895d01d
asman255 <smandth@gmail.com>
2024-11-29 15:22:38 +0700

    fix home

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.sendStatus("API Working");
+  res.status(200).send("API is Working");
 });
 
 app.listen(port, () => {
