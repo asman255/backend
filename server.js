2606025f5b9ae2c09ddf21c556f91676b2a84e68
asman255 <smandth@gmail.com>
2024-12-02 13:58:31 +0700

    fix url cor

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ connectCloudinary();
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://artnakkk-frontend-admin.vercel.app",
-  "http://localhost:5175", // For local development
+  "https://frontend-admin-psi-green.vercel.app",
+  "http://localhost:5173", // For local development
   "http://localhost:5176", // For local development
 ];
 
